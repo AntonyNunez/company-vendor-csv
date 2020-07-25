fa18f107b507b6c8fb4a3273ea9379d405d3ffc5
AntonyNunez <67538439+AntonyNunez@users.noreply.github.com>
2020-07-25 10:45:14 +0200

    initial commit

---
+++ src/main/java/com/exercise/companyvendor/CompanyVendorApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.companyvendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyVendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyVendorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/companyvendor/CompanyVendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.companyvendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyVendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
